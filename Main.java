39f20a615eca743650489d6b4ffc30a9b4158d84
Muhammad Farhan F M <59555308+FX24@users.noreply.github.com>
2021-09-20 08:13:41 +0000

    Implement SOLID and add some into Readme.md

--- Main.java
+++ Main.java
@@ -1,25 +1,40 @@
 import java.util.Scanner;
 
-class Main {
+class CalculateLength {
   public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        double x1,x2,y1,y2;
+    Calculate calculateService = new Calculate();
+    Input inputService = new Input();
 
-        System.out.print("x1: ");
-        x1 = scan.nextDouble();
-        System.out.print("y1: ");
-        y1 = scan.nextDouble();
-        System.out.print("x2: ");
-        x2 = scan.nextDouble();
-        System.out.print("y2: ");
-        y2 = scan.nextDouble();
+    double[]arr = inputService.getInput();
+    double length = calculateService.calculateLength(arr[0],arr[1],arr[2],arr[3]);
+    System.out.println(length);
+  }
+}
 
-        double x = x2-x1;
-        double y = y2-y1;
+class Calculate {
+  public double calculateLength (double x1,double y1,double x2,double y2) {    
+        double x = Math.pow(x2-x1,2);
+        double y = Math.pow(y2-y1,2);
+        return Math.pow((x+y) , 0.5);
+  }
+}
 
-        double length = Math.pow(x,2) + Math.pow(y,2);
-        length = Math.pow(length, 0.5);
-        System.out.println(length);
-    }
+class Input {
+  public double[] getInput() {
+    Scanner scan = new Scanner(System.in);
+    double x1,x2,y1,y2;
+    
+    System.out.print("x1: ");
+    x1 = scan.nextDouble();
+    System.out.print("y1: ");
+    y1 = scan.nextDouble();
+    System.out.print("x2: ");
+    x2 = scan.nextDouble();
+    System.out.print("y2: ");
+    y2 = scan.nextDouble();
+
+    double[]arr = {x1,y1,x2,y2};
+    return arr;
+  }
 }
---
+++ README.md
@@ -0,0 +1,22 @@
+# Problem Statement
+As a fan of geometry, Client want to model a line based on points consisting of (x, y) coordinates using the cartesian system. So that Client can calculate its length.
+
+# Requirement
+** JDK **: 11.0  
+** JUnit **: At least Version 3.0
+
+## Installation
+### Windows
+https://docs.oracle.com/en/java/javase/13/install/installation-jdk-microsoft-windows-platforms.html
+
+### Linux
+https://docs.oracle.com/en/java/javase/11/install/installation-jdk-linux-platforms.html
+
+# Instruction
+## Run Test
+
+
+## Run App
+```bash
+
+```
